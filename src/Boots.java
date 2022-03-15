c2ad78d8665d935d6d8bf4fdbf950d6e35e31820
13475 <eqweew>
2022-03-15 19:00:28 -0400

    Push 1

--- src/Boots.java
+++ src/Boots.java
@@ -1,3 +1,17 @@
 public class Boots extends Shoes {
 
+    private String soleSize;
+
+    public Boots(String color, int size, int price, String soleSize){
+        super(color, size, price);
+        this.soleSize = soleSize;
+    }
+
+    public void raining(){
+        System.out.println("It is raining outside. Better put on my boots!");
+    }
+
+    public String getSoleSize() {
+        return soleSize;
+    }
 }
--- src/Sneakers.java
+++ src/Sneakers.java
@@ -2,5 +2,16 @@ public class Sneakers extends Shoes{
 
     private String brand;
 
-    public Sneakers(String )
+    public Sneakers(String color, int size, int price, String brand){
+        super(color, size, price);
+        this.brand = brand;
+    }
+
+    public void playBall(){
+        System.out.println("Time to go play basketball. Better put on my sneakers");
+    }
+
+    public String getBrand() {
+        return brand;
+    }
 }
